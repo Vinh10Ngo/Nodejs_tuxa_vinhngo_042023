6fbbedcff31e36853c6f5363c7da81c8d9580b3d
Ngo Van Phuc Vinh <phucvinhngovan@gmail.com>
2023-06-23 20:08:56 +0700

    b05-01-Exercise Search

--- b04/routes/backend/themes.js
+++ b04/routes/backend/themes.js
@@ -18,16 +18,23 @@ router.get('/form', function(req, res, next) {
 
 router.get('(/:status)?', function(req, res, next) {
   let objWhere = {}
+  let keyword = paramsHelpers.getParams(req.query, 'keyword', '')
+  console.log(keyword)
   let currentStatus = paramsHelpers.getParams(req.params, 'status', 'all')
   let statusFilter = utilsHelpers.createFilterStatus(currentStatus)
-
-  if(currentStatus !== 'all') objWhere = {status: currentStatus}
+  if(currentStatus === 'all') {
+    if(keyword !== '') objWhere = {name: new RegExp(keyword, 'i')}
+  } else {
+    objWhere = {status: currentStatus, name: new RegExp(keyword, 'i')}
+  }
   
   itemsModel.find(objWhere).then((items) => {
     res.render('themes/list', { 
       pageTitle: 'Book Manager:: List',
       items: items, 
       statusFilter: statusFilter,
+      currentStatus,
+      keyword
     });
   })
  

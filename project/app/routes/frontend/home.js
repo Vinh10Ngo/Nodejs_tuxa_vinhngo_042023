018d588d5a3387a470eb8e8693ab1370e1240414
Ngo Van Phuc Vinh <phucvinhngovan@gmail.com>
2023-11-04 11:32:34 +0700

    b14-init theme

--- project/app/routes/frontend/home.js
+++ project/app/routes/frontend/home.js
@@ -15,5 +15,57 @@ router.get('/blog-detail-01', function(req, res, next) {
     layout: layoutNews
   });
 });
+router.get('/blog-detail-02', function(req, res, next) {
+  res.render(`${folderViewsNews}blog-detail-02`, { 
+    layout: layoutNews
+  });
+});
+router.get('/blog-grid', function(req, res, next) {
+  res.render(`${folderViewsNews}blog-grid`, { 
+    layout: layoutNews
+  });
+});
+router.get('/blog-list-01', function(req, res, next) {
+  res.render(`${folderViewsNews}blog-list-01`, { 
+    layout: layoutNews
+  });
+});
+router.get('/blog-list-02', function(req, res, next) {
+  res.render(`${folderViewsNews}blog-list-02`, { 
+    layout: layoutNews
+  });
+});
+router.get('/category-01', function(req, res, next) {
+  res.render(`${folderViewsNews}category-01`, { 
+    layout: layoutNews
+  });
+});
+router.get('/category-02', function(req, res, next) {
+  res.render(`${folderViewsNews}category-02`, { 
+    layout: layoutNews
+  });
+});
+router.get('/about', function(req, res, next) {
+  res.render(`${folderViewsNews}about`, { 
+    layout: layoutNews
+  });
+});
+router.get('/contact', function(req, res, next) {
+  res.render(`${folderViewsNews}contact`, { 
+    layout: layoutNews
+  });
+});
+router.get('/home-02', function(req, res, next) {
+  res.render(`${folderViewsNews}home-02`, { 
+    layout: layoutNews
+  });
+});
+router.get('/home-03', function(req, res, next) {
+  res.render(`${folderViewsNews}home-03`, { 
+    layout: layoutNews
+  });
+});
+
+
 
 module.exports = router;

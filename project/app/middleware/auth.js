3d2f0c4e1a33ca3c353491afca762af5210c4a56
Ngo Van Phuc Vinh <phucvinhngovan@gmail.com>
2023-12-30 18:06:12 +0700

    completed

--- project/app/middleware/auth.js
+++ project/app/middleware/auth.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
       }
 
     } else {
-            next()
-        // res.redirect(`${linkRedirect}login`)
+            // next()
+        res.redirect(`${linkRedirect}login`)
     }
 }
